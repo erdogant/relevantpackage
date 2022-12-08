1b91ed806c789c97daa46a1eea20d04b4c62fd76
erdogant <erdogant@gmail.com>
2022-12-08 16:14:59 +0100

    update cleaning file

---
+++ make_clean.sh
@@ -0,0 +1,21 @@
+echo "Cleaning repo first.."
+rm -rf dist
+rm -rf build
+rm -rf relevantpackage.egg-info
+rm -rf __pycache__
+rm -rf .pytest_cache
+rm -rf relevantpackage/__pycache__
+rm -rf relevantpackage/.pylint.d
+rm -rf tests/__pycache__
+rm -rf relevantpackage/utils/__pycache__
+rm -rf .pylint.d
+rm -rf relevantpackage/data/*.zip
+rm -rf relevantpackage/data/*.csv
+rm -rf relevantpackage/data/*.jpg
+rm -rf relevantpackage/relevantpackage/*.html
+rm -rf *.js
+rm -rf *.html
+rm -rf *.css
+rm -rf *.dot
+rm -rf .coverage*
+rm -rf .pkl
